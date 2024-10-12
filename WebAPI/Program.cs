69d4c5739b1385f449a28160231f360f4511b2f2
Yusuf Erdoğan <136724876+yusuferdogan38@users.noreply.github.com>
2024-10-12 15:50:21 +0300

    ı deleted consoleUI

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ICarService,CarManager>();
 builder.Services.AddSingleton<ICarDal, EfCarDal>();
+builder.Services.AddSingleton<ICustomerService,CustomerManager>();
+builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
